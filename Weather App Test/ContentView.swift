c94108fa87dca82f3a06be65acc14eb7fe475787
Chanuka Dilhara Mullevidana <62568997+chanukaHub@users.noreply.github.com>
2023-10-28 19:46:12 +0530

    Initial Commit

---
+++ Weather App Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App Test
+//
+//  Created by Chanuka Mullevidana on 2023-10-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather App Test/Weather_App_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_TestApp.swift
+//  Weather App Test
+//
+//  Created by Chanuka Mullevidana on 2023-10-28.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
